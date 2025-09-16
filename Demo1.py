a21fec89c2e918421bc6ba7c7e977c0b6a42b094
Patrick-Bouley <patrick.bouley@edu.sait.ca>
2025-09-16 09:44:52 -0600

    Second commit

---
+++ Demo1.py
@@ -0,0 +1,10 @@
+x = 3
+y = 4.5
+message = "hello"
+msg = 'hola'
+
+print(x)
+print(y)
+print(message)
+print(msg)
+z = 3
---
+++ mydemo1.py
@@ -0,0 +1,13 @@
+Msg = 'Greetings,'
+Name = "Patrick"
+Age = 26
+Weight = 158.4
+Tall = False
+Short = True
+
+print(Msg,Name) 
+print(Age)
+print(Weight)
+print(Tall)
+print(Short)
+
