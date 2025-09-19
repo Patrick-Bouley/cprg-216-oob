a2b547c0fa4e5c3da24719278cf68f356175a0b6
Patrick-Bouley <patrick.bouley@edu.sait.ca>
2025-09-19 16:41:03 -0600

    Week 3 Demo

---
+++ Week3.py
@@ -0,0 +1,7 @@
+print("welcome to my app...")
+name = input("What is your name?")
+print("Hello,", name) 
+Birth = input("What is your birth year?")
+year = int(Birth)
+age = 2025 - year
+print(age)
