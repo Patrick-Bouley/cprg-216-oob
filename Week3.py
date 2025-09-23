34e4a8556771530824fc3708ed2986ba66201afa
Patrick-Bouley <patrick.bouley@edu.sait.ca>
2025-09-23 09:55:23 -0600

    Week 3 review

--- Week3.py
+++ Week3.py
@@ -1,7 +1,14 @@
 print("welcome to my app...")
-name = input("What is your name?")
+name = input("What is your name? ")
 print("Hello,", name) 
-Birth = input("What is your birth year?")
+Birth = input("What is your birth year? ")
 year = int(Birth)
 age = 2025 - year
 print(age)
+'''
+Anything between the 3 single comments key
+is a form of commentation that
+allows you to use multiple lines
+for either explaining things
+or for not using hashtags on multiple lines 
+'''
